1c4744bbd55a4c3550a65b5850ed8fb69a5f85e8
Jacquin Moon <jacquindev@outlook.com>
2024-10-22 18:55:22 +0700

    add powershell function

--- dotposh/Modules/Get-Modules.ps1
+++ dotposh/Modules/Get-Modules.ps1
@@ -22,14 +22,14 @@ function Get-Modules {
     )
 
     if ($PSBoundParameters.Count -eq 0) {
-        $result = Get-InstalledModule | Select-Object Name, Version, Author, Description | Format-Table -AutoSize
+        $result = Get-InstalledModule | Select-Object Name, Version, Author, InstalledDate, Description | Format-Table -AutoSize
     }
 
     if ($output) {
         $result = Get-InstalledModule |
-        Select-Object Name, Version, Author, CompanyName, Description |
+        Select-Object Name, Version, Author, InstalledDate, Description |
         ConvertTo-Json -Depth 100 |
-        Out-File "$Env:DOTFILES\modules.lock.json" -Encoding utf8
+        Out-File "$Env:DOTFILES\modules.lock.json" -Encoding utf8 -Force
     }
 
     if ($list) {
@@ -37,7 +37,7 @@ function Get-Modules {
             $result = Get-Content "$Env:DOTFILES\modules.lock.json" | ConvertFrom-Json | Format-Table -AutoSize
         }
         else {
-            $result = Get-InstalledModule | Select-Object Name, Version, Author, Description | Format-Table -AutoSize
+            $result = Get-InstalledModule | Select-Object Name, Version, Author, InstalledDate, Description | Format-Table -AutoSize
         }
     }
 
